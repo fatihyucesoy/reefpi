87b972e3f565d92b529fe3dd503a27e6e9232cbe
graeme_milligan@hotmail.com <graeme_milligan@hotmail.com@b4711e58-09dc-bb7b-9a84-48cf063e47af>
2014-03-28 21:23:28 +0000

    updated the sql interface. should really be a singleton.

--- trunk/Controller/sensors/tempSensor/tempSensorSimulator.py
+++ trunk/Controller/sensors/tempSensor/tempSensorSimulator.py
@@ -9,25 +9,34 @@ class tempSimulator:
 	_maxReading = 100
 	_deviceId = None
 	_period = 0
+	_host = "localhost"
+	_user = "root"
+	_passwd = ""
+	_dataBase = "reefPi_RPi_schema"
+	_DB = None
 	
-	def __init__(self, probeId, probeName, minReading, maxReading, deviceId, period):
+	def __init__(self, probeId, probeName, minReading, maxReading, deviceId, period, \
+				host, user, passwd, dataBase):
 		self._probeId = probeId
 		self._probeName = probeName
 		self._minReading = minReading
 		self._maxReading = maxReading
 		self._deviceId = deviceId
 		self._period = period
+		self._host = host
+		self._user = user
+		self._passwd = passwd
+		self._dataBase = dataBase
+		self._DB = SQLInterface(self._host, self._user, self._passwd, self._dataBase)
 		
 	def _processNewReading(self):
-		DB     = SQLInterface()
-		
-		DB.insertSensorReading(self._probeId, self._reading)
+		self._DB.insertSensorReading(self._probeId, self._reading)
 		if(self._reading < self._minReading):
 			#DB.turnHeaterOn(_getCommandID(turnHeaterOn))
-			DB.addCommand(1, self._deviceId)
+			self._DB.addCommand(1, self._deviceId)
 		else:
 			#DB.turnHeateroff(_getCommandID(turnHeaterOff))
-		 	DB.addCommand(2, self._deviceId)	
+		 	self._DB.addCommand(2, self._deviceId)	
 		
 	def getProbeId(self):
 		return self._probeId
