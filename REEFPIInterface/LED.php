a952cdddf6bf03adc33c1dddd0528a0c3e712eda
graeme_milligan <graeme_milligan@hotmail.com>
2014-04-01 00:19:00 +0000

    updated graph functionality

--- REEFPIInterface/LED.php
+++ REEFPIInterface/LED.php
@@ -20,14 +20,17 @@
 <?php include('includes/dbconnect.php'); ?>
 
 <?php
+	date_default_timezone_set('Europe/London');
 	$result = mysqli_query($con,"SELECT timeStamp, reading FROM sensorReadings 
 					WHERE idsensor = (SELECT idsensor FROM sensor WHERE sensorName = 'tempSensor1') 
-					ORDER BY timeStamp DESC LIMIT 1000;");
+					ORDER BY timeStamp DESC LIMIT 100;");
 					
 	$dataArray = array(array('timeStamp', 'reading'));
 	
 	while($row = mysqli_fetch_array($result)) {
-    	$dataArray[] = array($row['timeStamp'], (float) $row['reading']);
+		$format = 'Y-m-d H:i:s';
+		$timestamp = DateTime::createFromFormat($format, $row['timeStamp']);
+    	$dataArray[] = array($timestamp->format('H:i:s'), (float) $row['reading']);
 	}
 	
 	
@@ -35,13 +38,17 @@
 ?>
 <!--Load the AJAX API-->
 
-        <script type="text/javascript" src="https://www.google.com/jsapi"></script>
-   		<script type="text/javascript">
+    <script type="text/javascript" src="https://www.google.com/jsapi"></script>
+   	<script type="text/javascript">
       	google.load("visualization", "1", {packages:["corechart"]});
       	google.setOnLoadCallback(drawChart);
       	function drawChart() {
+      	
         var data = google.visualization.arrayToDataTable(<?php echo json_encode($dataArray); ?>);
-
+		var formatter = new google.visualization.DateFormat({pattern: "HH:mm:ss"});
+		var formatter_short = new google.visualization.DateFormat({formatType: 'short'});
+		formatter_short.format(data, 0);
+		
         var options = {
         	height : 400,
           	title: 'temp 1 temperature',
@@ -49,8 +56,10 @@
           	curveType: 'function',
           	explorer:{ actions: ['dragToZoom', 'rightClickToReset'] , axis: 'horizontal'}
         };
-
+        
+		
         var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+        
         chart.draw(data, options);
       }
     </script>
@@ -77,7 +86,7 @@ LED Page
 <br>
 <h3>LED Temperature Reading</h3>
 <?php
-			$resultReadingLED = mysqli_query($con,"SELECT reading FROM sensorReadings 
+			$resultReadingLED = mysqli_query($con,"SELECT reading, timeStamp FROM sensorReadings 
 					WHERE idsensor = (SELECT idsensor FROM sensor WHERE sensorName = 'tempSensor1') 
 					ORDER BY timeStamp DESC 
 					limit 1;");
@@ -85,12 +94,17 @@ LED Page
 		echo "<table border='1'>
 	<tr>
 		<th>Reading</th>
+		<th>date</th>
+		<th>other date</th>
 	</tr>";
 
 			while($rowLED = mysqli_fetch_array($resultReadingLED))
 				{
+					$format = 'Y-m-d H:i:s';
+					$timestamp = DateTime::createFromFormat($format, $rowLED['timeStamp']);					
 					echo "<tr>";
 					echo "<td>" . $rowLED['reading'] . "</td>";
+					echo "<td>" . $timestamp->format('H:i:s'). "</td>";
 					echo "</tr>";
 				}
 		echo "</table>";
