4c91ee50170f7db5def34dab183b4733b05c9d35
graeme_milligan <graeme_milligan@hotmail.com>
2014-04-16 17:21:27 +0000

    major update. Code runs but is not adding events.  Also added base class framework for deviceBase

--- Controller/core.py
+++ Controller/core.py
@@ -5,8 +5,8 @@ import inspect
 
 from DBInterface.SQLInterface import *
 
-from sensors.tempSensor.tempSensorSimulator import *
-from sensors.tempSensor.DS18B20Interface import *
+from devices.tempSensor.tempSensorSimulator import *
+from devices.tempSensor.DS18B20Interface import *
 
 from devices.heater.heaterSimulator import *
 from devices.lighting.LEDSimulator import *
@@ -15,10 +15,10 @@ from scheduler.reefPI_Scheduler import *
 
 from lib.LEDIntensityCalculator import *
 from lib.scheduledEvent import *
-from lib.sensorAction import *
+from lib.deviceAction import *
 from lib.command import *
 
-latitude = -19.770621   # + to N  Defualt - (-19.770621) Heart Reef, Great Barrier Reef, QLD, Australia 
+latitude = -19.770621   # + to N  Defualt - (-19.770621) Heart Reef, Great Barrier Reef, QLD, Australia
 longitude = 149.238532  # + to E  Defualt - (149.238532)
 TimeZone = 10             # + to E  Defulat - (10)
 
@@ -26,23 +26,23 @@ host = "localhost"
 user = "root"
 passwd = ""
 dataBase = "reefPi_RPi_schema"
-		
+
 def createSensor(sensorInfo, DB):
 	#get a list of all of the actions for this sensor.  This could be empty
 	actionList = []
 	sensor = None
-	
-	
+
+
 	# get the actions for this sensor
 	for action in DB.getAllSensorActions(sensorInfo['idsensor']):
 		actionList.append(sensorAction(action))
-	
+
 	sensorClass = globals()[sensorInfo['sensorTypeName']]
 	sensor = sensorClass(sensorInfo, actionList, host, user, passwd, dataBase)
-	#create a sensor object of the correct type		
+	#create a sensor object of the correct type
 	return sensor
-			
-	
+
+
 def getAllSensors(DB):
 	sensorList = []
 	# get all sensors from the DB
@@ -52,22 +52,23 @@ def getAllSensors(DB):
 		sensor = createSensor(dbSensor, DB)
 		if(sensor):
 			sensorList.append(sensor)
-	
+
 	return sensorList
 
 def createDevice(deviceInfo, DB):
 	#get a list of all of the actions for this sensor.  This could be empty
 	actionList = []
 	device = None
-	
-	
+
+
 	# get the actions for this sensor
-	for action in DB.getAllDeviceCommandActions(sensorInfo['idsensor']):
-		actionList.append(sensorAction(action))
-	
-	devieClass = globals()[deviceInfo['deviceTypeName']]
-	device = deviceClass(deviceInfo, DB)
-	#create a sensor object of the correct type		
+	for action in DB.getAllDeviceActions(deviceInfo['iddevice']):
+		actionList.append(deviceAction(action))
+
+	deviceClass = globals()[deviceInfo['deviceTypeName']]
+	print deviceClass
+	device = deviceClass(deviceInfo, actionList, DB)
+	#create a sensor object of the correct type
 	return device
 
 def getAllDevices(DB):
@@ -76,25 +77,25 @@ def getAllDevices(DB):
 	for dbDevice in dbDeviceList:
 		deviceList.append(createDevice(dbDevice, DB))
 	return deviceList
-		
-		
-	
-		
+
+
+
+
 def initDevices(DB):
 	devices = getAllDevices(DB)
 	for device in devices:
-		deivce.init()
+		device.init()
 
-def init(DB):	
+def init(DB):
 	addTestData(DB)
-	initDevices(DB)
+	#initDevices(DB)
 	scheduledEvents = []
 	dbEventList = DB.getAllScheduledEvents()
 	for dbEvent in dbEventList:
 		scheduledEvents.append(scheduledEvent(dbEvent))
-	
+
 	return scheduledEvents
-			
+
 
 
 #
@@ -108,47 +109,54 @@ def addTestData(DB):
 	DB.addController('PCA', 'this is the type to represnt the PCA I2C type controller', 1)
 	DB.addControllerType('OneWireBus', 'this is the type to represent the one wire bus')
 	DB.addController('DIO', 'this is the type to represnt the PCA I2C type controller', 2)
-	
-	
+
+
 	DB.addDeviceCommand('turnOn', 'turn the device on')
 	DB.addDeviceCommand('turnOff', 'turn the device off')
 	DB.addDeviceCommand('setOutput', 'set the output value to that given (uses the value entry)')
 	DB.addDeviceCommand('getReading', 'get the reading from a sensor type device.  Will automatically \
-						 trigger any actions linked to the device depending on the coditions set in the action')	
-	
-	DB.addDeviceType('heaterSimulator', 'I2C')
-	DB.addDeviceType('LEDSimulator', 'I2C')
-	
-	DB.addDeviceType('tempSimulator', 'SW')
-	
-	
+						 trigger any actions linked to the device depending on the coditions set in the action')
+
+	DB.addBusType('I2C', "standard I2C bus")
+	DB.addBusType('SW', "used by represent simulated bus")
+
+	DB.addDeviceType('heaterSimulator', 1)
+	DB.addDeviceType('LEDSimulator', 2)
+
+	DB.addDeviceType('tempSimulator', 2)
+
+
 	DB.addDevice('heater1', 1, '0x40', 0, 1, 0)
 	DB.addDevice('LEDFAN', 1, '0x48', 0, 1, 0)
 	DB.addDevice('LEDChannel1', 2, '0x4A', 0, 1, 0)
 	DB.addDevice('LEDChannel2', 2, '0x4F', 0, 1, 0)
 	DB.addDevice('LEDChannel3', 2, '0x50', 0, 1, 0)
-	
-	
-	DB.addSensorActionType('crossing', 'Run the action only once when the condition is met')
-	DB.addSensorActionType('cont', 'Call the action continuously (every period) is the condition is met')
-	DB.addSensorActionRelation('lt', '<', "less than")
-	DB.addSensorActionRelation('gt', '>', "greater than")
-	DB.addSensorActionRelation('eq', '=', "equal to")
-	DB.addSensorActionRelation('neq', '!-', "lnot equal to")
+
+
+	DB.addDeviceActionType('crossing', 'Run the action only once when the condition is met')
+	DB.addDeviceActionType('cont', 'Call the action continuously (every period) is the condition is met')
+	DB.addDeviceActionRelation('lt', '<', "less than")
+	DB.addDeviceActionRelation('gt', '>', "greater than")
+	DB.addDeviceActionRelation('eq', '=', "equal to")
+	DB.addDeviceActionRelation('neq', '!-', "lnot equal to")
+
+
 	# create a temp sensor simulator to represent a tank heater
-	DB.addDevice('tempSensor1', 3, 'SW', 0, 2, 0)
-	DB.addSensorAction(1, 25, 1, 1, 1, 1)
-	DB.addSensorAction(1, 25, 2, 1, 1, 2)
-	
+	DB.addDevice('tempSensor1', 3, 'SW', 0, 1, 0)
+	DB.addDeviceAction(25, 1, 1, 1, 1, '')
+	DB.addDeviceAction(25, 2, 1, 1, 2, '')
+
 	# create a temp sensor simulator to represent the LED fans
-	DB.addSensor('tempSensor2', 1, 'SW', 'celcius', 4)
-	DB.addSensorAction(2, 27, 2, 1, 2, 1)
-	DB.addSensorAction(2, 27, 1, 2, 2, 2)
-	
+	DB.addDevice('tempSensor2', 3, 'SW', 0, 1, 0)
+	DB.addDeviceAction(27, 2, 1, 2, 1, '')
+	DB.addDeviceAction(27, 1, 2, 2, 2, '')
+
 	DB.addScheduleType("crone", "crone task based on standard crone syntax")
 	DB.addScheduleType("interval", "task will run at regular intervals with period defined here")
-	
-	
+
+	DB.addScheduledEvent("TempSensor1Sample", 2, 6, 4, None, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
+						 None, None, None, None, None, None, None, 5)
+
 	DB.addScheduledEvent("croneEvent", 1, 3, 1, 100, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
 						 None, None, None, None, None, None, None, 6)
 	DB.addScheduledEvent("croneEvent", 1, 3, 2, 0, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
@@ -156,10 +164,10 @@ def addTestData(DB):
 	DB.addScheduledEvent("intervalEvent", 2, 4, 2, 0, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
 						 None, None, None, None, None, None, None, 10)
 	DB.addScheduledEvent("intervalEvent", 2, 4, 1, 0, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
-						 None, None, None, None, None, None, None, 10)	
+						 None, None, None, None, None, None, None, 10)
 	DB.addScheduledEvent("intervalEvent", 2, 5, 3, 255, datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"), \
-						 None, None, None, None, None, None, None, 2)	
-	
+						 None, None, None, None, None, None, None, 2)
+
 
 #
 # Uses the current data time to get the intensity value
@@ -169,17 +177,17 @@ def getLEDIntensity():
 	return calculateSunLight(BluePWMHigh[0], BluePWMLow[0], BlueFull[0], \
 						datetime.datetime.now(), \
 						latitude, longitude, \
-						TimeZone)	
+						TimeZone)
 
 #
-# gets the next command from the command table in the 
+# gets the next command from the command table in the
 # db and decodes it.  It then calls the command
-# on the selected device	
+# on the selected device
 #
 def processCommand(DB):
 	result = None
 	command = None
-	
+
 	# check for a command form the DB
 	dbCommand = DB.getNextCommand()
 	if(dbCommand != None):
@@ -199,19 +207,19 @@ def processCommand(DB):
 					result = methodPointer()
 				elif(paramLength == 1):
 					result = methodPointer(command.args)
-				
+
 			else:
 				print "failed to run method {0} on device {1}".format(command.deviceCommand, command.deviceName)
-			
+
 		except Exception, e:
 			print "failed to run method {0} on device {1}: {2}".format(command.deviceCommand, command.deviceName, e)
-			
+
 	return result
 
-		
+
 def decodeSchedulerEvent(deviceId, probeID, command, level):
 	print datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S") + \
-			":Scheduled event running: " + str(command) +' ' + str(deviceId)	
+			":Scheduled event running: " + str(command) +' ' + str(deviceId)
 	DB = SQLInterface(host, user, passwd, dataBase)
 	DB.addCommand(deviceId, command, [level])
 
@@ -224,13 +232,7 @@ def addScheduledEvents(scheduler, scheduledEvents):
 		elif(event.scheduleTypeName == 'interval'):
 			scheduler.AddIntervalEvent(decodeSchedulerEvent, event)
 
-#
-# 
-#
-#
-def checkForSystemUpdates(sensors, sensorPool,  devices, scheduledEvents, DB):
-	sensor = sensors
-	
+
 def main():
 	scheduledEvents = []
 	DB = SQLInterface(host, user, passwd, dataBase)
@@ -239,23 +241,20 @@ def main():
 	addScheduledEvents(scheduler, scheduledEvents)
 	scheduler.Run()
 
-	
+
 	#Main loop.  When happy this will be switched to a while(1) or similar
 	# for now its easier to limit number of loops
 	while(1):
 	#for loop in range(1,100):
 		while(processCommand(DB)):
 			pass
-			
-		#TODO scan the db for changes such as new devices or events
-		checkForSystemUpdates(sensors, sensorPool,  devices, scheduledEvents, DB)
+		print"looping"
 		time.sleep(1)
-	
-	
-	print 'Going to sleep'	
+
+
+	print 'Going to sleep'
 	for process in sensorPool:
 		process.terminate()
-
 
 if __name__ == "__main__":
     main()
--- Controller/devices/heater/heaterSimulator.py
+++ Controller/devices/heater/heaterSimulator.py
@@ -9,28 +9,30 @@ class heaterSimulator:
 	address = None
 	level = None
 	_DB = None
-	
-	def __init__(self, deviceInfo, host, user, passwd, dataBase):
+	_actionList = None
+
+	def __init__(self, deviceInfo, actionList, DB):
 		self.Iddevice = deviceInfo['iddevice']
 		self.deviceName = deviceInfo['deviceName']
 		self.iddeviceType = deviceInfo['iddeviceType']
-		self.address = deviceInfo['address']
-		self.status = deviceInfo['status']
-		self.level = deviceInfo['level']
+		self.address = deviceInfo['deviceAddress']
+		self.status = deviceInfo['deviceStatus']
+		self._actionList = actionList
 		# TODO: reaffirm the state on start up.  This makes sure things stay in sync
 		# over power cycles.
-		self._DB = SQLInterface(host, user, passwd, dataBase)
-	
+		self._DB = DB
+		self._setState(self.status)
+
 	def _setState(self, state):
 		self._DB.setDeviceStatus(self.iddevice, state)
 		self.status = state
 		return self.status
-		
+
 	def turnOn(self):
 		print self.deviceName +': Turning heater on'
 		return self._setState(1)
-	
+
 	def turnOff(self):
 		print self.deviceName + ': Turning heater off'
 		return self._setState(0)
-		
+
